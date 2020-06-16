6f0106068839890d1e0ea95cc85a5efe993498d1
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-16 22:54:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,15 +16,17 @@ end
 def get_japanese_emoticon(file_path, emoticons)
   # code goes here
   emoticon_hash = load_library(file_path)
-  # emoticon_hash.each do |key, value|
-  #   value.each do |inner_key, inner_value|
-  #     if emoticons == inner_value
-  #       key
-  #     else
-  #       nil
-  #     end
-  #   end
-  # end
+  emoticons_list = load_library(file_path)
+emoji = emoticons
+  emoticons_list.each do|key, value|
+    value.each do|inner_key, inner_value|
+      binding.pry
+      if emoji == inner_value
+         return
+       end
+    end
+  end
+  "Sorry, that emoticon was not found"
 
 end
 
