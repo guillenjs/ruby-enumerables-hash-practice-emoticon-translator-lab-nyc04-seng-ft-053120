7bebd1a143bc7eeae6c90adcd2d43a9070ce854c
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-16 22:41:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,7 +38,7 @@ emoji = emoticons
       if emoji == inner_value
         return key
       else
-        "Sorry, that emoticon was not found"
+          puts "Sorry, that emoticon was not found"
       end
     end
   end
