ac3bc19dbd724f6941edd4d27d99d7d843f540e9
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-16 22:35:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,43 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+    emoticons.each_with_object({}) do |(key, value), result|
+        if !result[key]
+          result[key] = {"english": value[0], "japanese": value[1] }
+        end
+        result
+      end
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticons)
   # code goes here
+  emoticon_hash = load_library(file_path)
+  # emoticon_hash.each do |key, value|
+  #   value.each do |inner_key, inner_value|
+  #     if emoticons == inner_value
+  #       key
+  #     else
+  #       nil
+  #     end
+  #   end
+  # end
+
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticons)
   # code goes here
+
+  emoticons_list = load_library(file_path)
+emoji = emoticons
+  emoticons_list.each do|key, value|
+    value.each do|inner_key, inner_value|
+      while emoji == inner_value
+        puts key
+      end
+    end
+  end
 end
